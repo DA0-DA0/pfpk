db202c3d417e694a4462648efad8548cfe70a88c
Noah Saso <noahsaso@gmail.com>
2023-02-04 13:11:16 -0800

    Remove loop queries, and allow tokens an address has staked with a voting contract.

--- src/chains/cw721.ts
+++ src/chains/cw721.ts
@@ -2,36 +2,54 @@ import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { GetOwnedNftImageUrlFunction } from "../types";
 import { KnownError, NotOwnerError } from "../error";
 import * as Cw721 from "../cw721";
+import * as DaoVotingCw721Staked from "../daoVotingCw721Staked";
 
-export const getOwnedNftImageUrl = (rpc: string, walletAddress: string): GetOwnedNftImageUrlFunction => async (
-  _,
-  collectionAddress,
-  tokenId
-) => {
-  let imageUrl: string | undefined;
-  try {
-    const client = await CosmWasmClient.connect(rpc);
+export const getOwnedNftImageUrl =
+  (rpc: string, walletAddress: string): GetOwnedNftImageUrlFunction =>
+  async (_, collectionAddress, tokenId) => {
+    let imageUrl: string | undefined;
+    try {
+      const client = await CosmWasmClient.connect(rpc);
 
-    const owner = await Cw721.getOwner(client, collectionAddress, tokenId);
-    // If does not own NFT, throw error.
-    if (owner.owner !== walletAddress) {
-      throw new NotOwnerError();
+      const owner = await Cw721.getOwner(client, collectionAddress, tokenId);
+      // If wallet does not directly own NFT, check if staked with a DAO voting
+      // module.
+      if (owner !== walletAddress) {
+        const isStakingContract = await DaoVotingCw721Staked.isContract(
+          client,
+          owner
+        );
+        if (isStakingContract) {
+          const addressStakedToken =
+            await DaoVotingCw721Staked.addressStakedToken(
+              client,
+              owner,
+              collectionAddress,
+              tokenId
+            );
+
+          if (!addressStakedToken) {
+            throw new NotOwnerError();
+          }
+        } else {
+          throw new NotOwnerError();
+        }
+      }
+
+      imageUrl = await Cw721.getImageUrl(client, collectionAddress, tokenId);
+    } catch (err) {
+      // If error already handled, pass up the chain.
+      if (err instanceof KnownError || err instanceof NotOwnerError) {
+        throw err;
+      }
+
+      console.error(err);
+      throw new KnownError(
+        500,
+        "Unexpected error retrieving NFT info from chain",
+        err
+      );
     }
 
-    imageUrl = await Cw721.getImageUrl(client, collectionAddress, tokenId);
-  } catch (err) {
-    // If error already handled, pass up the chain.
-    if (err instanceof KnownError || err instanceof NotOwnerError) {
-      throw err;
-    }
-
-    console.error(err);
-    throw new KnownError(
-      500,
-      "Unexpected error retrieving NFT info from chain",
-      err
-    );
-  }
-
-  return imageUrl;
-};
+    return imageUrl;
+  };
---
+++ src/daoVotingCw721Staked.ts
@@ -0,0 +1,59 @@
+import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+
+type InfoResponse = {
+  info: {
+    contract: string;
+    version: string;
+  };
+};
+
+export const isContract = async (
+  client: CosmWasmClient,
+  contractAddress: string
+): Promise<boolean> => {
+  const info: InfoResponse = await client.queryContractSmart(contractAddress, {
+    info: {},
+  });
+
+  return (
+    !!info &&
+    "contract" in info &&
+    info.contract === "crates.io:dao-voting-cw721-staked"
+  );
+};
+
+// Get all NFTs an address has staked and check if the token ID is in the list.
+const LIMIT = 30;
+export const addressStakedToken = async (
+  client: CosmWasmClient,
+  contractAddress: string,
+  address: string,
+  tokenId: string
+): Promise<boolean> => {
+  const tokens: string[] = [];
+  while (true) {
+    const response: string[] = await client.queryContractSmart(
+      contractAddress,
+      {
+        staked_nfts: {
+          address,
+          start_after: tokens[tokens.length - 1],
+          limit: LIMIT,
+        },
+      }
+    );
+
+    if (!response?.length) {
+      break;
+    }
+
+    tokens.push(...response);
+
+    // If we have less than the limit of items, we've exhausted them.
+    if (response.length < LIMIT) {
+      break;
+    }
+  }
+
+  return tokens.includes(tokenId);
+};
