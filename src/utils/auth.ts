bb9a98eb1b441eb42719f9c09dfb8b07ada3d725
Noah Saso <noahsaso@gmail.com>
2024-07-26 16:56:43 -0400

    support injective signature verification

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,7 @@
 import { makeSignDoc, serializeSignDoc } from '@cosmjs/amino'
+import { keccak256 } from '@cosmjs/crypto'
+import { fromBase64, toBech32 } from '@cosmjs/encoding'
+import secp256k1 from 'secp256k1'
 
 import {
   secp256k1PublicKeyToBech32Address,
@@ -13,12 +16,8 @@ export const verifySignature = async ({
   data,
   signature,
 }: RequestBody): Promise<boolean> => {
-  try {
-    const signer = secp256k1PublicKeyToBech32Address(
-      data.auth.publicKey,
-      data.auth.chainBech32Prefix
-    )
-    const message = serializeSignDoc(
+  const getMessage = (signer: string): Uint8Array =>
+    serializeSignDoc(
       makeSignDoc(
         [
           {
@@ -45,6 +44,36 @@ export const verifySignature = async ({
       )
     )
 
+  // Injective uses different address derivation and signature verification.
+  if (data.auth.chainId === 'injective-1') {
+    try {
+      // https://github.com/InjectiveLabs/injective-ts/blob/5f44b7796441749711c170bf3ebdcbed2664bb5a/packages/sdk-ts/src/core/accounts/PublicKey.ts#L67-L84
+      const decompressed = injectiveDecompressPubKey(data.auth.publicKey)
+      const addressBuffer = Buffer.from(
+        keccak256(Buffer.from(decompressed, 'hex'))
+      ).subarray(-20)
+
+      const signer = toBech32(data.auth.chainBech32Prefix, addressBuffer)
+      const message = getMessage(signer)
+
+      return secp256k1.ecdsaVerify(
+        fromBase64(signature),
+        keccak256(message),
+        Buffer.from(data.auth.publicKey, 'hex')
+      )
+    } catch (err) {
+      console.error('Injective signature verification', err)
+      return false
+    }
+  }
+
+  try {
+    const signer = secp256k1PublicKeyToBech32Address(
+      data.auth.publicKey,
+      data.auth.chainBech32Prefix
+    )
+    const message = getMessage(signer)
+
     return await verifySecp256k1Signature(
       data.auth.publicKey,
       message,
@@ -54,6 +83,23 @@ export const verifySignature = async ({
     console.error('Signature verification', err)
     return false
   }
+}
+
+// https://github.com/InjectiveLabs/injective-ts/blob/6e25b3f156d964666db8bc7885df653166aac523/packages/sdk-ts/src/utils/crypto.ts#L70-L84
+const injectiveDecompressPubKey = (startsWith02Or03: string) => {
+  // if already decompressed an not has trailing 04
+  const testBuffer = Buffer.from(startsWith02Or03, 'hex')
+
+  if (testBuffer.length === 64) startsWith02Or03 = '04' + startsWith02Or03
+
+  let decompressed = Buffer.from(
+    secp256k1.publicKeyConvert(Buffer.from(startsWith02Or03, 'hex'), false)
+  ).toString('hex')
+
+  // remove trailing 04
+  decompressed = decompressed.substring(2)
+
+  return decompressed
 }
 
 // Middleware to protect routes with authentication. If it does not return, the
